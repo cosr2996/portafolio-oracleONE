dfbbf6254a253f3809a241bf2520261165e33951
cosr2996 <115452805+cosr2996@users.noreply.github.com>
2023-07-03 15:13:37 -0600

    se termino el diseno de la pagina

--- js/validacion.js
+++ js/validacion.js
@@ -1,1 +1,25 @@
 //Haz tú validación en javascript acá
+
+//DOM
+const nombre = document.querySelector('.nombre')
+const email = document.querySelector('.email')
+const asunto = document.querySelector('asunto')
+const mensaje = document.querySelector('.mensaje')
+console.log(nombre.value)
+
+nombre.addEventListener('blur',(e)=>{
+    if(nombre.validity.valid){
+        nombre.classList.remove("error")
+    }else{
+         nombre.classList.add("error")
+         
+    }
+})
+
+email.addEventListener('blur',(e)=>{
+    if(email.validity.valid){
+        email.classList.remove("error")
+    }else{
+         email.classList.add("error")
+    }
+})
